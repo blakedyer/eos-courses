75130c580d822e0010101b3a993adf97e5600429
blakedyer <blake.c.dyer@gmail.com>
2024-11-14 16:52:39 -0800

    test reveal on mtd

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Teaching'
+project = " "
 copyright = '2024, Blake Dyer'
 author = 'Blake Dyer'
 release = '2024'
@@ -14,8 +14,19 @@ release = '2024'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["crate.sphinx.csv", "sphinx.ext.mathjax"]
+extensions = ["myst_nb", "crate.sphinx.csv", "sphinx.ext.mathjax"]
 
+nb_execution_mode = "off"
+
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_image",
+]
+
+myst_url_schemes = ("http", "https", "mailto")
 # templates_path = ['_templates']
 exclude_patterns = []
 
@@ -24,7 +35,7 @@ exclude_patterns = []
 
 html_theme = "sphinx_book_theme"
 html_theme_options = {
-    "github_url": "https://github.com/blakedyer/eos-courses",
+    # "github_url": "https://github.com/blakedyer/eos-courses",
     "show_prev_next": False,
     # "navbar_end": ["search-field.html", "navbar-icon-links.html"],
     "logo": {
@@ -35,4 +46,4 @@ html_theme_options = {
 #     "**": [],
 # }
 html_favicon = 'favicon.png'
-# html_static_path = ['_static']
+html_static_path = ['_static','eos240-public/Lectures']
