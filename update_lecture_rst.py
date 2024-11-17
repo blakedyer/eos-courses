ba223b2401bce2c716a40f0dd1ffd604ca6b3399
blakedyer <blake.c.dyer@gmail.com>
2024-11-17 13:03:20 -0800

    automated lectures and course updates

---
+++ update_lecture_rst.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import os
+import json
+
+courses = ["eos240", "eos408", "eos423"]
+
+for c in courses:
+    prefix = f"source/{c}-public/Lectures/"
+
+    slides = [s for s in os.listdir(prefix) if 'slides' in s]
+    slides.sort()
+    raw = [f"{s.split('.slides')[0]}.ipynb" for s in slides]
+
+    a = json.load(open(f'{prefix}{raw[0]}',))
+    long_title = a['cells'][0]['source'][0]
+
+    lectures = ""
+    for s in slides:
+        lectures += f"""{s.split('.slides')[0]}.rst
+   """
+        
+    lecture_index = f"""Lecture slides
+==========================
+
+.. toctree::
+   :maxdepth: 1
+
+   {lectures}
+"""
+
+    with open(f"{prefix.split('Lectures/')[0]}/lectures.rst", "w+") as f:
+            f.writelines(lecture_index)
+
+    for i in range(len(slides)):
+        a = json.load(open(f'{prefix}{raw[i]}',))
+        long_title = a['cells'][0]['source'][0]
+        slide_file=slides[i]
+        s = slide_file.split('.slides')[0]
+        
+        slide_page = f"""{long_title}
+=====================================================   
+
+Links for full screen web viewing
+------------------------------------------
+Spacebar to advance, shift+spacebar to go backwards, escape for overview.
+
+`{long_title} <../_static/{slide_file}>`_
+
+
+Links for PDF/printing
+------------------------
+
+To generate a PDF, click the link then go to print-->save as PDF in your browser.
+
+`{long_title} <../_static/{slide_file}?print-pdf>`_
+"""
+        with open(f"{prefix.split('Lectures/')[0]}/{s}.rst", "w+") as f:
+            f.writelines(slide_page)
+
+
+
+
+    
